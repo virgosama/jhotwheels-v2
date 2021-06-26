883a27bd9307fe3186ae4779a2d01b449c738cfb
Maria SS <virgo.sama@gmail.com>
2021-06-26 16:52:48 +0700

    trying to deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BloggerService} from './blogger.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'jhotwheels-v2';
+
+  constructor(private bloggerService: BloggerService) {
+  }
+
+  ngOnInit(): void {
+    this.bloggerService.getBlogPost().subscribe(resp => {
+      console.log(resp);
+    });
+  }
 }
